7a1af288ea12519c4747c6e1efec5c763aeddafd
Jason-6 <591726822@qq.com>
2023-08-19 13:04:44 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from fairseq.modules.lightweight_convolution import LightweightConv1d
-import torch.nn as nn
-
-conv1d = nn.Conv1d(256, 256, 15, 1, 7, groups=256)
-print(conv1d)
-lightweightconv1d = LightweightConv1d(256, kernel_size=12, padding=7)
-
-print(LightweightConv1d)
