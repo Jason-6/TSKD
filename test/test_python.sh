2ed91dd7afaa3e0443b26b04b4699937528213bf
Jason-6 <591726822@qq.com>
2023-08-19 13:03:37 +0800

    Delete test directory

--- test/test_python.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source tools/neural_sp/miniconda/bin/activate
-
-modules="neural_sp test utils setup.py"
-pycodestyle -r ${modules} --show-source --show-pep8 --ignore="E501"
-
-pytest
